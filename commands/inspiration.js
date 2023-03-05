14cdc56c77fec1a01f34fee80aafa3c1141676ff
Tanner <tannerkobylinski@gmail.com>
2023-03-04 21:00:32 -0600

    inspire switched to slashcommand

--- commands/inspiration.js
+++ commands/inspiration.js
@@ -4,9 +4,9 @@ const apiFunctions = require('../functions/apiFunctions');
 module.exports = [{
     name: 'inspire',
     description: 'Generate an inspirational quote!',
-    async execute(robot, message) {
+    async execute(robot, interaction) {
         //https://github.com/kishlaya/inspirobot-bot/blob/master/README.md
         let image = await apiFunctions.getAsync(robot, 'https://inspirobot.me/api?generate=true', null);
-        message.reply(image);
+        interaction.reply(image);
     }
 }];
