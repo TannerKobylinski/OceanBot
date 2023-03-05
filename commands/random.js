994c7a02ff0f0c96026ce22233496ca7615402ea
Tanner <tannerkobylinski@gmail.com>
2023-03-04 21:00:47 -0600

    roll switched to slashcommand

--- commands/random.js
+++ commands/random.js
@@ -1,16 +1,24 @@
 
 module.exports = [{
     name: 'roll',
-    description: 'Roll a N sided die (e.g. *roll N*); N defaults to 6',
-    async execute(robot, message, args) {
+    description: 'Roll an N sided die - N defaults to 6',
+    options: [
+        {
+            "name": "sides",
+            "description": "number of sides",
+            "type": 4,
+            "required": false,
+        }
+    ],
+    async execute(robot, interaction) {
+        let sides = interaction.options.getInteger('sides');
         let max = 6;
         let min = 1;
-        if(args.length > 0) {
-            let num = parseInt(args[0]);
-            if(Number.isInteger(num)) max = num;
-            else return message.reply("that's a weird number.");
+        if(sides) {
+            if(Number.isInteger(sides)) max = sides;
+            else return interaction.reply(`${sides}? that's a weird number.`);
         }
         let rand = Math.floor(Math.random() * (max - min + 1)) + min;
-        return message.reply(`**${rand}**`);
+        return interaction.reply(`**${rand}**`);
     },
 }];
