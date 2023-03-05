e37ae87d7a7b8f264a542f03794a3795af316852
Tanner <tannerkobylinski@gmail.com>
2023-03-04 21:00:57 -0600

    joke switched to slashcommand

--- commands/joke.js
+++ commands/joke.js
@@ -4,26 +4,35 @@ const apiFunctions = require('../functions/apiFunctions');
 module.exports = [{
     name: 'joke',
     description: 'Ask for a joke',
-    async execute(robot, message, args) {
+    options: [
+        {
+            "name": "keywords",
+            "description": "keywords to include",
+            "type": 3,
+            "required": false,
+        }
+    ],
+    async execute(robot, interaction) {
         let url = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist,sexist,explicit`;
-        if(args.length>0) url += `&contains=${args.join('%20')}`;
+        const userKeywords = interaction.options.getString('keywords');
+        if(userKeywords) url += `&contains=${userKeywords.replace(/\s/,'%20')}`;
         try {
             var response = await apiFunctions.getAsync(robot, url, {}, true);
             if(response.error) throw new Error(response.error);
-            if(response.type == 'twopart') await sendJokeTwoPart(message, response);
-            else sendJokeSingle(message, response);
+            if(response.type == 'twopart') await sendJokeTwoPart(interaction, response);
+            else sendJokeSingle(interaction, response);
         }
         catch (error) {
             console.log(error);
-            message.reply('no joke found!');
+            interaction.reply('no joke found!');
         }
     },
 }];
 
-function sendJokeSingle(message, response){
-    message.channel.send(response.joke);
+function sendJokeSingle(interaction, response){
+    interaction.reply(response.joke);
 }
 
-async function sendJokeTwoPart(message, response){
-    message.channel.send(`${response.setup}\n||${response.delivery}||`);
+async function sendJokeTwoPart(interaction, response){
+    interaction.reply(`${response.setup}\n||${response.delivery}||`);
 }
