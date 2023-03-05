be06006d6d8c8f1ea55211eb11a8629e7547d78d
Tanner <tannerkobylinski@gmail.com>
2023-03-04 21:38:21 -0600

    react switched to slash command

--- commands/react.js
+++ commands/react.js
@@ -1,34 +1,42 @@
 const reactFunctions = require('../functions/reactFunctions');
-const { getMessageReferenced } = require('../functions/utils');
 
 module.exports = [{
     name: 'react',
-    description: 'React to the latest channel message with the given characters',
-    async execute(robot, message, args, options) {
-        let messageToReactTo = await getMessageReferenced(message); //grab referenced message if it exists
-
-        if(messageToReactTo.id == message.id) { //if we didnt get referenced message, find last message in channel
-            let messages = await message.channel.messages.fetch({ limit: 2 });
-            messageToReactTo = messages.last();
+    description: 'React to the latest channel message with the given alphabet characters',
+    type: 1,
+    options: [
+        {
+            "name": "phrase",
+            "description": "phrase to react with",
+            "type": 3,
+            "required": false,
         }
+    ],
+    async execute(robot, interaction) {
+        let phrase = interaction.options.getString('phrase');
+        let messages = await interaction.channel.messages.fetch({ limit: 1 });
+        let messageToReactTo = messages.last();
 
         // React to message if retrieved
         if(!messageToReactTo) return;
-        if(args.length < 1){
-            console.info(`${message.author.username} reacting to ${messageToReactTo.content}`);
+        await interaction.deferReply();
+
+        if(!phrase || phrase.length < 1){
+            console.info(`${interaction.user.username} reacting to ${messageToReactTo.content}`);
             reactFunctions.reactWithRandomEmoji(messageToReactTo);
         }
         else {
-            const reactLetters = args.join('');
+            const reactLetters = phrase.split(/\s+/).join('');
+            console.log(reactLetters);
             let letters = reactFunctions.getReactableLetters(reactLetters, true);
+            if(!letters) return interaction.editReply('Invalid phrase! Must be alphabet characters!');
             if(letters) {
                 if(letters.length > 20) letters = letters.slice(0, 20);
-                console.info(`${message.author.username} reacting to ${messageToReactTo.content}`);
+                console.info(`${interaction.user.username} reacting to ${messageToReactTo.content}`);
                 await reactFunctions.reactToMessageWithLetters(messageToReactTo, letters);
             }
         }
-        message.delete().then(msg => console.log(`Deleted message from ${msg.author.username}`)).catch(err => {
-            console.info(`Unable to delete react message in ${message.guild.name}`);
-        });
+        await interaction.editReply(':)');
+        await interaction.deleteReply();
     }
 }];
